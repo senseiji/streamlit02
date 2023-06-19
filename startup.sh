62bf404478650fc3f84c9e08192d0aa23b263826
senseiji <131722925+senseiji@users.noreply.github.com>
2023-06-19 21:04:38 +0000

    my first strealit code

---
+++ startup.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+# demo this
+# streamlit hello  --server.enableCORS false --server.enableXsrfProtection false
+streamlit run  my_streamlit.py  --server.enableCORS false --server.enableXsrfProtection false
